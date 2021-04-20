370e192c665e7cb8f79a55ff09de1ac3cb2c043e
Viktor590 <resident1081994@gmail.com>
2021-04-20 11:02:31 +0300

    added section header

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+$ (function () {
+  $('.header-partners__slider').slick({
+  dots: false,
+  nextArrow: false,
+  prevArrow: false,
+  infinite: true,
+  speed: 200,
+  autoplay: true,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+ });
+});
